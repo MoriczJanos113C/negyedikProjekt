ba06624003ba4ae40417103267ba9c0f8f4ec881
LevaiLevente321 <levike-2000@freemail.hu>
2021-05-05 10:06:06 +0200

    Kerület/terület kész.

--- app/src/main/java/com/example/negyedikprojekt/KeruletTerulet.java
+++ app/src/main/java/com/example/negyedikprojekt/KeruletTerulet.java
@@ -1,8 +1,53 @@
 package com.example.negyedikprojekt;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class KeruletTerulet extends AppCompatActivity {
+
+    // 2*a + 2*b
+    // a*b
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.kerureltterulet);
+
+        EditText aoldal = findViewById(R.id.aoldal);
+        EditText boldal = findViewById(R.id.boldal);
+
+        Button kerulet = findViewById(R.id.kerulet);
+        Button terulet = findViewById(R.id.terulet);
+
+        kerulet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i =  new Intent(KeruletTerulet.this, Final.class);
+
+                int a = Integer.parseInt(aoldal.getText().toString());
+                int b = Integer.parseInt(boldal.getText().toString());
+                i.putExtra("eredmeny", 2*(a+b));
+
+                startActivity(i);
+            }
+        });
+
+        terulet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i =  new Intent(KeruletTerulet.this, Final.class);
+
+                int a = Integer.parseInt(aoldal.getText().toString());
+                int b = Integer.parseInt(boldal.getText().toString());
+                i.putExtra("eredmeny", a*b);
+
+                startActivity(i);
+            }
+        });
+    }
 }
